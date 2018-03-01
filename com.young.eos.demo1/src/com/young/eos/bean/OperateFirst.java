d9f7d0b84b0a916f54c041852b6c00d628016249
young <xujian_999@163.com>
2018-03-01 12:46:24 +0800

    项目提交

---
+++ com.young.eos.demo1/src/com/young/eos/bean/OperateFirst.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.young.eos.bean;
+
+/**
+ * <pre>
+ * Title: 程序的中文名称
+ * Description: 程序功能的描述
+ * </pre>
+ * @author Young
+ * @version 1.00.00
+ * 
+ */
+/*
+ * 修改历史
+ * $log$
+ */
+public class OperateFirst {
+
+	/**
+	 * 
+	 */
+	public OperateFirst() {
+		// TODO 自动生成的构造函数存根
+	}
+
+}
